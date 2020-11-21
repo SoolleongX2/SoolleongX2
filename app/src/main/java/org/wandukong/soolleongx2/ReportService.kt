0898fff274eea1a6bf5083637f515340005fca70
yjooooo <thsduswn4141@naver.com>
2020-11-22 07:35:32 +0900

    Updated

---
+++ app/src/main/java/org/wandukong/soolleongx2/ReportService.kt
@@ -0,0 +1,12 @@
+package org.wandukong.soolleongx2
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+
+
+interface ReportService {
+
+}
